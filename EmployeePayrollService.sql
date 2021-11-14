8d94295833b6e123b1795768d309983c3ed3414c
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-11-14 19:08:10 +0530

    Create table and show table

--- EmployeePayrollService.sql
+++ EmployeePayrollService.sql
@@ -5,6 +5,10 @@ create database payroll_service;
 show databases;
 use payroll_service;
 
+-- UC2
+create table employee_payroll(id int not null auto_increment primary key, empname varchar(25),salary double,startDate date);
+desc employee_payroll;
+
 
 
 
